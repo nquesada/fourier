e42f3708cbc7770e43b211d4a93439b8d1bb17fe
Nicolas Quesada <zeitus@gmail.com>
2019-07-27 23:39:47 -0400

    Added readme and docstrings

--- fourier.py
+++ fourier.py
@@ -1,19 +1,38 @@
 import numpy as np
 
-def fourier_approx(x, y, scale = True):
-	N = len(x)
-	assert N == len(y)
-	dx = x[1]-x[0]
-	assert np.allclose(np.diff(x), dx*np.ones(N-1))
-	yt = np.fft.fftshift(np.fft.fft(y,norm="ortho"))
-	xt = np.fft.fftshift(np.fft.fftfreq(len(x),dx))*2*np.pi
-	if N%2 ==0:
-		s = 0.0
-	else:
-		s = 0.5
-	phases = np.exp(-1j*np.pi*(np.arange(N)-0.5+(np.arange(N)+s)/N))
-	yt = phases*yt
-	if scale:
-		dk = xt[1]-xt[0]
-		yt = np.sqrt(dx/dk)*yt
-	return xt, yt
+
+def fourier_approx(x, y, scale=True):
+    r"""Calculates the fourier transform of the function y = f(x) where x is 
+        a vector of equally spaced values. The fourier transform follows the conventions 
+        of numpy:
+        
+        F(k) = \frac{1}{\sqrt{2 \pi}} \int dx e^{i k x} f(x)
+        
+        and also is unitary meaning that
+        
+        \sum |F(k)|^2 dk \approx \int |F(k)|^2 dk =  \int |f(x)|^2 dx \approx \sum |f(x)|^2 dx
+        Args:
+              x (array): vector of abscissae, grid the function f(x) is evaluated
+              y (array): vector of ordinates, y = f(x)
+              scale (bool): Makes the transform unitary with respect to integration as above.
+                            Otherwise it guarantees that \sum |F(k)|^2 = \sum |f(x)|^2 
+        
+        Returns:
+             tuple (k, F(k)): The abcissae (k) and ordinate F(k) of the fourier transform of y = f(x)
+        """
+    N = len(x)
+    assert N == len(y)
+    dx = x[1] - x[0]
+    assert np.allclose(np.diff(x), dx * np.ones(N - 1))
+    yt = np.fft.fftshift(np.fft.fft(y, norm="ortho"))
+    xt = np.fft.fftshift(np.fft.fftfreq(len(x), dx)) * 2 * np.pi
+    if N % 2 == 0:
+        s = 0.0
+    else:
+        s = 0.5
+    phases = np.exp(-1j * np.pi * (np.arange(N) - 0.5 + (np.arange(N) + s) / N))
+    yt = phases * yt
+    if scale:
+        dk = xt[1] - xt[0]
+        yt = np.sqrt(dx / dk) * yt
+    return xt, yt
--- test_fourier.py
+++ test_fourier.py
@@ -3,21 +3,24 @@ import numpy as np
 from fourier import fourier_approx
 
 
-def norm_gaussian(x,mu=1.0, k0=0.0):
-    return np.exp(-0.5*(x*mu)**2)/np.sqrt(np.sqrt(np.pi)/mu)*np.exp(1j*k0*x)
+def norm_gaussian(x, mu=1.0, k0=0.0):
+    return (
+        np.exp(-0.5 * (x * mu) ** 2)
+        / np.sqrt(np.sqrt(np.pi) / mu)
+        * np.exp(1j * k0 * x)
+    )
 
 
-
-@pytest.mark.parametrize("N", [400,401])
-@pytest.mark.parametrize("mu", [0.5,1,2])
-@pytest.mark.parametrize("k0", [-1,0,1])
-@pytest.mark.parametrize("x0", [-1,0,1])
-def test_gaussian(mu,k0,x0,N):
-	xf = 40
-	x = np.linspace(-xf,xf,N)
-	dx = x[1]-x[0]
-	y = norm_gaussian(x-x0,mu=mu,k0=k0)
-	xt, yt = fourier_approx(x,y)
-	exact = np.exp(-1j*k0*x0)*norm_gaussian(xt-k0, mu = 1.0/mu,k0 = -x0)
-	#print(np.max(np.abs(yt-exact)), k0,x0)
-	assert np.allclose(yt, exact)
+@pytest.mark.parametrize("N", [400, 401])
+@pytest.mark.parametrize("mu", [0.5, 1, 2])
+@pytest.mark.parametrize("k0", [-1, 0, 1])
+@pytest.mark.parametrize("x0", [-1, 0, 1])
+def test_gaussian(mu, k0, x0, N):
+    xf = 40
+    x = np.linspace(-xf, xf, N)
+    dx = x[1] - x[0]
+    y = norm_gaussian(x - x0, mu=mu, k0=k0)
+    xt, yt = fourier_approx(x, y)
+    exact = np.exp(-1j * k0 * x0) * norm_gaussian(xt - k0, mu=1.0 / mu, k0=-x0)
+    # print(np.max(np.abs(yt-exact)), k0,x0)
+    assert np.allclose(yt, exact)
